eecc8d1ef6511c8437405462399f650ed835f20c
Khiem Nguyen <ng.khiem97vn@gmail.com>
2022-05-30 05:11:14 -0400

    update trainning

---
+++ models/resnet.py
@@ -0,0 +1,84 @@
+import torch.nn as nn
+import torch
+import torch.nn.functional as F
+
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, in_planes, planes, stride=1):
+        super(BasicBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != self.expansion*planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, self.expansion*planes, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(self.expansion*planes)
+            )
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
+
+
+class ResNet(nn.Module):
+    def __init__(self, num_classes=10, init_weights=True):
+        super(ResNet, self).__init__()
+        self.in_planes = 64
+
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.layer1 = self._make_layer(BasicBlock, 64, 2, stride=1)  # output[64, 56, 56]
+        self.layer2 = self._make_layer(BasicBlock, 128, 2, stride=2) # output[128, 27, 27]
+        self.layer3 = self._make_layer(BasicBlock, 256, 2, stride=2) # output[256, 13, 13]
+        self.layer4 = self._make_layer(BasicBlock, 512, 2, stride=2) # output[512, 6, 6]
+
+        self.classifier = nn.Sequential(
+            nn.Dropout(p=0.5),
+            nn.Linear(512 * 6 * 6, 2048),
+            nn.ReLU(inplace=True),
+            nn.Dropout(p=0.5),
+            nn.Linear(2048, 2048),
+            nn.ReLU(inplace=True),
+            nn.Linear(2048, num_classes),
+        )
+        if init_weights:
+            self._initialize_weights()
+
+    def _make_layer(self, block, planes, num_blocks, stride):
+        strides = [stride] + [1]*(num_blocks-1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, stride))
+            self.in_planes = planes * block.expansion
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = F.avg_pool2d(out, 4)
+        out = out.view(out.size(0), -1)
+        out = self.classifier(out)
+        return out
+
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, 0, 0.01)
+                nn.init.constant_(m.bias, 0)
---
+++ train_resnet.py
@@ -0,0 +1,147 @@
+import os
+import sys
+import json
+
+import torch
+import torch.nn as nn
+from torchvision import transforms, datasets, utils
+import matplotlib.pyplot as plt
+import numpy as np
+import torch.optim as optim
+from tqdm import tqdm
+
+from models.resnet import ResNet
+import time
+
+def train(model, device, train_loader, validate_loader, optimizer_type, lr, epochs):
+    print(f"Training start with {optimizer_type} and lr={lr}")
+    model.to(device)
+    loss_function = nn.CrossEntropyLoss()
+
+    if optimizer_type == 'SGD':
+        optimizer = optim.SGD(model.parameters(), lr=lr)
+    elif optimizer_type == 'Adam':
+        optimizer = optim.Adam(model.parameters(), lr=lr)
+    else:
+        raise ValueError("optimizer_type should be 'SGD' or 'Adam'")
+
+    since = time.time()
+    save_path = './models/ResNet_'+optimizer_type+'_'+str(lr).replace(".", "-")+'.pth'
+    best_acc = 0.0
+    train_steps = len(train_loader)
+    training_loss = []
+    val_accuracy = []
+    for epoch in range(epochs):
+        model.train()
+        running_loss = 0.0
+        train_bar = tqdm(train_loader, file=sys.stdout)
+        for step, data in enumerate(train_bar):
+            images, labels = data
+            optimizer.zero_grad()
+            outputs = model(images.to(device))
+            loss = loss_function(outputs, labels.to(device))
+            loss.backward()
+            optimizer.step()
+            running_loss += loss.item()
+
+            # print statistics
+            train_bar.desc = "train epoch[{}/{}] loss:{:.3f}".format(epoch + 1,
+                                                                     epochs,
+                                                                     loss)
+
+        # validate
+        model.eval()
+        acc = 0.0  # accumulate accurate number / epoch
+        with torch.no_grad():
+            val_bar = tqdm(validate_loader, file=sys.stdout)
+            for val_data in val_bar:
+                val_images, val_labels = val_data
+                outputs = model(val_images.to(device))
+                predict_y = torch.max(outputs, dim=1)[1]
+                acc += torch.eq(predict_y, val_labels.to(device)).sum().item()
+
+        val_accurate = acc / validate_loader.dataset.__len__()
+        print('[epoch %d] train_loss: %.3f  val_accuracy: %.3f' %
+              (epoch + 1, running_loss / train_steps, val_accurate))
+        training_loss.append(running_loss / train_steps)
+        val_accuracy.append(val_accurate)
+
+        if val_accurate > best_acc:
+            best_acc = val_accurate
+            torch.save(model.state_dict(), save_path)
+
+    print(f'Finished training for {optimizer_type} and lr={lr}')
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(
+        time_elapsed // 60, time_elapsed % 60))
+    
+    with open('./log/training_ResNet_'+optimizer_type+'_'+str(lr).replace(".", "-")+'.txt', 'w') as f:
+        f.write('Training complete in {:.0f}m {:.0f}s\n'.format(time_elapsed // 60, time_elapsed % 60))
+        f.write(f'Best val_accuracy: {best_acc}\n')
+        f.write(f'Training loss: {training_loss}\n')
+        f.write(f'Validation accuracy: {val_accuracy}\n')
+
+
+def main():
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    print("using {} device.".format(device))
+
+    data_transform = {
+        "train": transforms.Compose([transforms.RandomResizedCrop(224),
+                                     transforms.RandomHorizontalFlip(),
+                                     transforms.ToTensor(),
+                                     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]),
+        "val": transforms.Compose([transforms.Resize((224, 224)),
+                                   transforms.ToTensor(),
+                                   transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])}
+
+    data_root = os.getcwd()
+    image_path = os.path.join(data_root, "dataset")
+    assert os.path.exists(image_path), "{} path does not exist.".format(image_path)
+    train_dataset = datasets.ImageFolder(root=os.path.join(image_path, "train"),
+                                         transform=data_transform["train"])
+    train_num = len(train_dataset)
+
+    # {
+    #     "0": "face",
+    #     "1": "face_mask"
+    # }
+    categories_list = train_dataset.class_to_idx
+    categories_dict = dict((val, key) for key, val in categories_list.items())
+    print("categories_dict: {}".format(categories_dict))
+
+    # write dict into json file
+    json_str = json.dumps(categories_dict, indent=4)
+    with open('categories.json', 'w') as json_file:
+        json_file.write(json_str)
+
+    batch_size = 32
+    nw = min([os.cpu_count(), batch_size if batch_size > 1 else 0, 8])  # number of workers
+    print('Using {} dataloader workers every process'.format(nw))
+
+    train_loader = torch.utils.data.DataLoader(train_dataset,
+                                               batch_size=batch_size, shuffle=True,
+                                               num_workers=nw)
+
+    validate_dataset = datasets.ImageFolder(root=os.path.join(image_path, "val"),
+                                            transform=data_transform["val"])
+    val_num = len(validate_dataset)
+    validate_loader = torch.utils.data.DataLoader(validate_dataset,
+                                                  batch_size=4, shuffle=False,
+                                                  num_workers=nw)
+
+    print("using {} images for training, {} images for validation.".format(train_num,
+                                                                           val_num))
+
+    net = ResNet(num_classes=2, init_weights=True)
+
+    learning_path = './learning.json'
+    assert os.path.exists(learning_path), "file: '{}' dose not exist.".format(learning_path)
+    with open(learning_path, "r") as f:
+        learning = json.load(f)
+    for learning_item in learning:
+        for lr in learning[learning_item]["lr"]:
+            train(net, device, train_loader, validate_loader, learning_item, lr, 10)
+
+if __name__ == '__main__':
+    main()
