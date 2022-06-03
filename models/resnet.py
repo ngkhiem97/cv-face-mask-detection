9a7aecb85c8b5f1cda7c9997f10f1611077f62e0
Khiem Nguyen <ng.khiem97vn@gmail.com>
2022-06-03 04:53:19 -0400

    update

--- models/resnet.py
+++ models/resnet.py
@@ -3,20 +3,17 @@ import torch
 import torch.nn.functional as F
 
 class BasicBlock(nn.Module):
-    expansion = 1
-
-    def __init__(self, in_planes, planes, stride=1):
+    def __init__(self, input_planes, output_planes, stride=1):
         super(BasicBlock, self).__init__()
-        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(planes)
-        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
-        self.bn2 = nn.BatchNorm2d(planes)
-
+        self.conv1 = nn.Conv2d(input_planes, output_planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(output_planes)
+        self.conv2 = nn.Conv2d(output_planes, output_planes, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(output_planes)
         self.shortcut = nn.Sequential()
-        if stride != 1 or in_planes != self.expansion*planes:
+        if stride != 1 or input_planes != output_planes:
             self.shortcut = nn.Sequential(
-                nn.Conv2d(in_planes, self.expansion*planes, kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm2d(self.expansion*planes)
+                nn.Conv2d(input_planes, output_planes, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(output_planes)
             )
 
     def forward(self, x):
@@ -30,15 +27,13 @@ class BasicBlock(nn.Module):
 class ResNet(nn.Module):
     def __init__(self, num_classes=2, init_weights=True):
         super(ResNet, self).__init__()
-        self.in_planes = 64
-
+        self.input_planes = 64
         self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(64)
-        self.layer1 = self._make_layer(BasicBlock, 64, 2, stride=1)  # output[64, 56, 56]
-        self.layer2 = self._make_layer(BasicBlock, 128, 2, stride=2) # output[128, 27, 27]
-        self.layer3 = self._make_layer(BasicBlock, 256, 2, stride=2) # output[256, 13, 13]
-        self.layer4 = self._make_layer(BasicBlock, 512, 2, stride=2) # output[512, 6, 6]
-
+        self.layer1 = self._create_layer(64, 2, stride=1)  
+        self.layer2 = self._create_layer(128, 2, stride=2) 
+        self.layer3 = self._create_layer(256, 2, stride=2) 
+        self.layer4 = self._create_layer(512, 2, stride=2) 
         self.classifier = nn.Sequential(
             nn.Dropout(p=0.5),
             nn.Linear(512 * 7 * 7, 2048),
@@ -51,12 +46,11 @@ class ResNet(nn.Module):
         if init_weights:
             self._initialize_weights()
 
-    def _make_layer(self, block, planes, num_blocks, stride):
-        strides = [stride] + [1]*(num_blocks-1)
+    def _create_layer(self, output_planes, num_blocks, stride):
         layers = []
-        for stride in strides:
-            layers.append(block(self.in_planes, planes, stride))
-            self.in_planes = planes * block.expansion
+        for stride in [stride] + [1]*(num_blocks-1):
+            layers.append(BasicBlock(self.input_planes, output_planes, stride))
+            self.input_planes = output_planes
         return nn.Sequential(*layers)
 
     def forward(self, x):
